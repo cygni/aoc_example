fee470735fc56b85a8d6f5a72cbf62fd4e4b29aa
Torbjörn Gannholm <torbjorn.gannholm@gmail.com>
2021-11-13 21:11:17 +0100

    dart input

--- day12/app.dart
+++ day12/app.dart
@@ -1,20 +1,33 @@
-import 'dart:io' show Platform;
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
 
-String solutionPart1() {
-  return "1337";
+String solutionPart1(List<int> input) {
+  return input[0].toString();
 }
 
-String solutionPart2() {
-  return "42";
+String solutionPart2(List<int> input) {
+  return input[1].toString();
 }
 
-void main() {
+void main() async {
+  print('Dart');
+  List<int> input = await parseInput("input.txt");
   String part = Platform.environment["part"] ?? "part1";
   if (part == "part1") {
-    print(solutionPart1());
+    print(solutionPart1(input));
   } else if (part == "part2") {
-    print(solutionPart2());
+    print(solutionPart2(input));
   } else {
     print("Unknown part " + part);
   }
+}
+
+Future<List<int>> parseInput(String path) async {
+  return new File(path)
+      .openRead()
+      .transform(utf8.decoder)
+      .transform(const LineSplitter())
+      .map((line) => int.parse(line))
+      .toList();
 }
