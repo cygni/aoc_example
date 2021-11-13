19d1fc109b333c57416034140fcf8f9b47de7bb2
Torbjörn Gannholm <torbjorn.gannholm@gmail.com>
2021-11-13 18:03:55 +0100

    add inputs ballerina and clojure

--- day13/src/day13/core.clj
+++ day13/src/day13/core.clj
@@ -1,15 +1,24 @@
 (ns day13.core
+  (:require [clojure.java.io :as io])
   (:gen-class))
 
-(defn solution-part1 [] "1337")
+  (defn read-lines []
+    (with-open [rdr (io/reader "input.txt")]
+      (reduce conj [] (line-seq rdr))))
+  
+(defn parse-input [lines]
+  (mapv #(Integer/parseInt %) lines))
 
-(defn solution-part2 [] "42")
+(defn solution-part1 [input] (str (first input)))
+
+(defn solution-part2 [input] (str (nth input 1)))
 
 (defn -main
   "AOC example solution"
   [& args]
-  (let [part (or (System/getenv "part") "part1")]
+  (let [part (or (System/getenv "part") "part1")
+        input (parse-input (read-lines))]
     (println (condp = part
-               "part1" (solution-part1)
-               "part2" (solution-part2)
+               "part1" (solution-part1 input)
+               "part2" (solution-part2 input)
                (str "Unknown part " part)))))
