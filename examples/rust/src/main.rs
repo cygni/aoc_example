99ac76d129c7ef98a8a87f6b82625f6dac9b87d3
Mikael Elm <elm.micke@gmail.com>
2021-11-23 22:31:13 +0100

    Added input reading and adapted to example for Rust (#49)
    * Added input reading and adapted to example for Rust
    
    * Removed commented row

--- examples/rust/src/main.rs
+++ examples/rust/src/main.rs
@@ -1,15 +1,28 @@
 use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
 
-fn getSolutionPart1() -> i32 {
-    return 1337;
+fn get_solution_part1(parsed_input: &Vec<i32>) -> i32 {
+    return parsed_input.iter().sum();
 }
 
-fn getSolutionPart2() -> i32 {
-    return 42;
+fn get_solution_part2(parsed_input: &Vec<i32>) -> i32 {
+    return parsed_input.iter().product();
+}
+
+fn read_input_as_integers(path: &str) -> Vec<i32> {
+    let file = File::open(path).expect("not found");
+    let reader = BufReader::new(file);
+
+    return reader
+        .lines()
+        .map(|line| line.unwrap().parse::<i32>().unwrap())
+        .collect();
 }
 
 fn main() {
-
+    let parsed_input = read_input_as_integers("input.txt");
     let part = match env::var("part") {
         Ok(val) => val,
         Err(_e) => "part1".to_string(),
@@ -17,8 +30,8 @@ fn main() {
 
     println!("Rust");
     if part == "part2" {
-        println!("{}", getSolutionPart2());
+        println!("{}", get_solution_part2(&parsed_input));
     } else {
-        println!("{}", getSolutionPart1());
+        println!("{}", get_solution_part1(&parsed_input));
     }
 }
