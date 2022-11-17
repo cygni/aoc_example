8135562341854f5bd0aabfeecce39c18781311b4
Torbjörn Gannholm <torbjorn.gannholm@gmail.com>
2022-11-17 15:02:53 +0100

    Update clojure

--- examples/clojure/project.clj
+++ examples/clojure/project.clj
@@ -1,6 +1,6 @@
 (defproject day13 "0.1.0-SNAPSHOT"
   :description "clojure aoc example"
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot day13.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
